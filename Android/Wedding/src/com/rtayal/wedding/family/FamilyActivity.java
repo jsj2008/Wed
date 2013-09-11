47e77bb248d44892f727904f049574982f061380
Rishabh Tayal <rtayal11@gmail.com>
2013-09-11 12:15:37 -0500

    commit.

--- Android/Wedding/src/com/rtayal/wedding/family/FamilyActivity.java
+++ Android/Wedding/src/com/rtayal/wedding/family/FamilyActivity.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
@@ -35,6 +36,15 @@ public class FamilyActivity extends Activity {
 		listView = (ListView) findViewById(R.id.familyListView);
 
 		setupTabs();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	void downloadFamilyList(final Boolean thakurs) {
