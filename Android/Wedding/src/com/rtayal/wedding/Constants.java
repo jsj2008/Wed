16f316195ba381d0977cb72bbc1c7a972da64367
Rishabh Tayal <rtayal11@gmail.com>
2013-09-16 09:36:00 -0500

    maps android

--- Android/Wedding/src/com/rtayal/wedding/Constants.java
+++ Android/Wedding/src/com/rtayal/wedding/Constants.java
@@ -5,9 +5,10 @@ public class Constants {
 	public static String PRDropboxEventsScheduleURL = "https://dl.dropboxusercontent.com/u/57884865/wedding_app_files/schedule_list.plist";
 	public static String PRDropboxThakurFamilyURL = "https://dl.dropboxusercontent.com/u/57884865/wedding_app_files/thakurs_family.plist";
 	public static String PRDropboxTayalFamilyURL = "https://dl.dropboxusercontent.com/u/57884865/wedding_app_files/tayals_family.plist";
+	public static String PRDropboxVenuesURL = "https://dl.dropboxusercontent.com/u/57884865/wedding_app_files/venues.plist";
 
 	public static String kAPIHost = "http://appikon.com/";
 	public static String kAPIPath = "wedding_app/";
-	
+
 	public static String Localytics_App_Key = "dfd759fdfda91377340d12f-bb8a5478-1a55-11e3-1300-004a77f8b47f";
 }
--- Android/Wedding/src/com/rtayal/wedding/venues/VenuesActivity.java
+++ Android/Wedding/src/com/rtayal/wedding/venues/VenuesActivity.java
@@ -1,17 +1,74 @@
 package com.rtayal.wedding.venues;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.rtayal.wedding.Constants;
+import com.rtayal.wedding.PRPlistDownloader;
+import com.rtayal.wedding.PRPlistDownloader.DataDownloadListener;
 import com.rtayal.wedding.R;
 
 public class VenuesActivity extends Activity {
+
+	GoogleMap mapView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_venues);
+
+		mapView = ((MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+
+		PRPlistDownloader downloader = new PRPlistDownloader();
+		downloader.setDataDownloadListener(new DataDownloadListener() {
+
+			@Override
+			public void dataDownloadedSuccessfully(
+					ArrayList<HashMap<String, Object>> data) {
+				LatLng latLng = new LatLng(22.761062, 75.952130);
+				for (HashMap<String, Object> hashMap : data) {
+
+					latLng = new LatLng(Double.valueOf((String) hashMap
+							.get("Latitude")), Double.valueOf((String) hashMap
+							.get("Longitude")));
+					mapView.addMarker(new MarkerOptions().position(latLng)
+							.title((String) hashMap.get("Title")));
+				}
+
+				mapView.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,
+						11.0f));
+
+			}
+
+			@Override
+			public void dataDownloadFailed() {
+
+			}
+		});
+		downloader.execute(Constants.PRDropboxVenuesURL);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
