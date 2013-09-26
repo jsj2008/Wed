47c9a4687d31eaa43a5816ae24b9dcaf3a9fe79d
Rishabh Tayal <rtayal11@gmail.com>
2013-09-26 14:58:52 -0500

    commit.

--- iOS/Wed/PRButton.m
+++ iOS/Wed/PRButton.m
@@ -82,32 +82,32 @@
     //    [self drawGloss];
 }
 
--(void)drawGloss {
-    CGContextRef currentContext = UIGraphicsGetCurrentContext();
-    
-    CGGradientRef glossGradient;
-    CGColorSpaceRef rgbColorspace;
-    size_t num_locations = 2;
-    CGFloat locations[2] = { 0.0, 1.0 };
-    CGFloat components[8] = { 1.0, 1.0, 1.0, 0.35,  // Start color
-        1.0, 1.0, 1.0, 0.06 }; // End color
-    
-    rgbColorspace = CGColorSpaceCreateDeviceRGB();
-    glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
-    
-    //    CGRect currentBounds = self.bounds;
-    //    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
-    //    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
-    //    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
-    
-    //    CGAffineTransform scaleT = CGAffineTransformMakeScale(2, 1.0);
-    //    CGAffineTransform invScaleT = CGAffineTransformInvert(scaleT);
-    //    CGPoint invS = CGPointMake(invScaleT.a, invScaleT.d);
-    //    CGFloat radius = (self.bounds.size.width / 2) * invS.x;
-    
-    CGGradientRelease(glossGradient);
-    CGColorSpaceRelease(rgbColorspace);
-}
+//-(void)drawGloss {
+//    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+//
+//    CGGradientRef glossGradient;
+//    CGColorSpaceRef rgbColorspace;
+//    size_t num_locations = 2;
+//    CGFloat locations[2] = { 0.0, 1.0 };
+//    CGFloat components[8] = { 1.0, 1.0, 1.0, 0.35,  // Start color
+//        1.0, 1.0, 1.0, 0.06 }; // End color
+//
+//    rgbColorspace = CGColorSpaceCreateDeviceRGB();
+//    glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
+//
+//    //    CGRect currentBounds = self.bounds;
+//    //    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+//    //    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
+//    //    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
+//
+//    //    CGAffineTransform scaleT = CGAffineTransformMakeScale(2, 1.0);
+//    //    CGAffineTransform invScaleT = CGAffineTransformInvert(scaleT);
+//    //    CGPoint invS = CGPointMake(invScaleT.a, invScaleT.d);
+//    //    CGFloat radius = (self.bounds.size.width / 2) * invS.x;
+//
+//    CGGradientRelease(glossGradient);
+//    CGColorSpaceRelease(rgbColorspace);
+//}
 
 //-(void)animate {
 //    float randomDelay = ((float)rand() / RAND_MAX);
