96c7ab1ca15ff9fd9282e70af725e8883b314726
Rishabh Tayal <rtayal11@gmail.com>
2013-09-26 14:51:15 -0500

    commit.

--- iOS/Wed/PRButton.m
+++ iOS/Wed/PRButton.m
@@ -13,6 +13,9 @@
 {
     UIColor* originalBackgroundColor;
 }
+
+@property (nonatomic, strong) CALayer* glassLayer;
+
 @end
 
 @implementation PRButton
@@ -31,9 +34,9 @@
 
 - (void)drawRect:(CGRect)rect
 {
-//    if (!originalBackgroundColor) {
-//        originalBackgroundColor = self.backgroundColor;
-//    }
+    //    if (!originalBackgroundColor) {
+    //        originalBackgroundColor = self.backgroundColor;
+    //    }
     [self setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
     [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     UIFont *font = [UIFont fontWithName:PRFontHelveticaNeueLight size:16.0f];
@@ -45,36 +48,38 @@
     self.layer.borderColor = [[UIColor whiteColor] CGColor];
     
     //Add a translucent layer to uibutton layer. Set color of layer depending on uibutton tag.
-    CALayer*  layer = [CALayer layer];
-    layer.frame = self.bounds;
-    switch (self.tag) {
-        case 1:
-            layer.backgroundColor = [UIColor colorWithRed:212/255.0 green:76/255.0 blue:193/255.0 alpha:1].CGColor;
-            break;
-        case 2:
-            layer.backgroundColor = [UIColor colorWithRed:26/255.0 green:141/255.0 blue:225/255.0 alpha:1].CGColor;
-            break;
-        case 3:
-            layer.backgroundColor = [UIColor colorWithRed:255/255.0 green:38/255.0 blue:14/255.0 alpha:1].CGColor;
-            break;
-        case 4:
-            layer.backgroundColor = [UIColor colorWithRed:0/255.0 green:212/255.0 blue:35/255.0 alpha:1].CGColor;
-            break;
-        default:
-            break;
+    if (!_glassLayer) {
+        _glassLayer = [CALayer layer];
+        _glassLayer.frame = self.bounds;
+        switch (self.tag) {
+            case 1:
+                _glassLayer.backgroundColor = [UIColor colorWithRed:212/255.0 green:76/255.0 blue:193/255.0 alpha:1].CGColor;
+                break;
+            case 2:
+                _glassLayer.backgroundColor = [UIColor colorWithRed:26/255.0 green:141/255.0 blue:225/255.0 alpha:1].CGColor;
+                break;
+            case 3:
+                _glassLayer.backgroundColor = [UIColor colorWithRed:255/255.0 green:38/255.0 blue:14/255.0 alpha:1].CGColor;
+                break;
+            case 4:
+                _glassLayer.backgroundColor = [UIColor colorWithRed:0/255.0 green:212/255.0 blue:35/255.0 alpha:1].CGColor;
+                break;
+            default:
+                break;
+        }
+        _glassLayer.opacity = 0.4;
+        [self.layer insertSublayer:_glassLayer below:self.layer];
     }
-    layer.opacity = 0.4;
-    [self.layer insertSublayer:layer below:self.layer];
-
-    // Drawing code
-//    [self animate];
-
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    CGFloat outerMargin = 7.5f;
-//    CGRect outerRect = CGRectInset(self.bounds, outerMargin, outerMargin);
-//    drawCurvedGloss(context, outerRect, 180);
     
-//    [self drawGloss];
+    // Drawing code
+    //    [self animate];
+    
+    //    CGContextRef context = UIGraphicsGetCurrentContext();
+    //    CGFloat outerMargin = 7.5f;
+    //    CGRect outerRect = CGRectInset(self.bounds, outerMargin, outerMargin);
+    //    drawCurvedGloss(context, outerRect, 180);
+    
+    //    [self drawGloss];
 }
 
 -(void)drawGloss {
@@ -90,16 +95,16 @@
     rgbColorspace = CGColorSpaceCreateDeviceRGB();
     glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
     
-//    CGRect currentBounds = self.bounds;
-//    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
-//    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
-//    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
+    //    CGRect currentBounds = self.bounds;
+    //    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+    //    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
+    //    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
     
-//    CGAffineTransform scaleT = CGAffineTransformMakeScale(2, 1.0);
-//    CGAffineTransform invScaleT = CGAffineTransformInvert(scaleT);
-//    CGPoint invS = CGPointMake(invScaleT.a, invScaleT.d);
-//    CGFloat radius = (self.bounds.size.width / 2) * invS.x;
-
+    //    CGAffineTransform scaleT = CGAffineTransformMakeScale(2, 1.0);
+    //    CGAffineTransform invScaleT = CGAffineTransformInvert(scaleT);
+    //    CGPoint invS = CGPointMake(invScaleT.a, invScaleT.d);
+    //    CGFloat radius = (self.bounds.size.width / 2) * invS.x;
+    
     CGGradientRelease(glossGradient);
     CGColorSpaceRelease(rgbColorspace);
 }
@@ -107,21 +112,21 @@
 //-(void)animate {
 //    float randomDelay = ((float)rand() / RAND_MAX);
 //    [UIView animateWithDuration:3.0 delay:randomDelay options: UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse | UIViewAnimationOptionAllowUserInteraction animations:^{
-//        
+//
 //        int randomNumX = rand() % (10 - 7) + 7;
 ////        int randomNumY = rand() % (10 - 7) + 7;
-//        
+//
 //        self.center = CGPointMake(self.center.x + randomNumX, self.center.y);
 //    } completion:nil];
 //}
 
 //void drawCurvedGloss (CGContextRef context, CGRect rect, CGFloat radius)
-//{    
+//{
 //    CGColorRef glossStart = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.6].CGColor;
 //    CGColorRef glossEnd = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.1].CGColor;
-//    
+//
 //    CGMutablePathRef glossPath = CGPathCreateMutable();
-//    
+//
 //    CGContextSaveGState(context);
 //    CGPathMoveToPoint(glossPath, NULL, CGRectGetMidX(rect), CGRectGetMinY(rect)-radius+rect.size.height/2);
 //    CGPathAddArc(glossPath, NULL, CGRectGetMidX(rect), CGRectGetMinY(rect)-radius+rect.size.height/2, radius, 0.75f*M_PI, 0.25f*M_PI, YES);
@@ -130,16 +135,16 @@
 //    CGContextClip(context);
 //    CGContextAddPath(context, createRoundedRectForRect(rect, 6.0f));
 //    CGContextClip(context);
-//    
+//
 //    CGRect half = CGRectMake(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height/2);
-//    
-//    
+//
+//
 //    drawLinearGradient(context, half, glossStart, glossEnd);
 //    CGContextRestoreGState(context);
 //}
 //
 //CGMutablePathRef createRoundedRectForRect(CGRect rect, CGFloat radius) {
-//    
+//
 //    CGMutablePathRef path = CGPathCreateMutable();
 //    CGPathMoveToPoint(path, NULL, CGRectGetMidX(rect), CGRectGetMinY(rect));
 //    CGPathAddArcToPoint(path, NULL, CGRectGetMaxX(rect), CGRectGetMinY(rect), CGRectGetMaxX(rect), CGRectGetMaxY(rect), radius);
@@ -147,7 +152,7 @@
 //    CGPathAddArcToPoint(path, NULL, CGRectGetMinX(rect), CGRectGetMaxY(rect), CGRectGetMinX(rect), CGRectGetMinY(rect), radius);
 //    CGPathAddArcToPoint(path, NULL, CGRectGetMinX(rect), CGRectGetMinY(rect), CGRectGetMaxX(rect), CGRectGetMinY(rect), radius);
 //    CGPathCloseSubpath(path);
-//    
+//
 //    return path;
 //}
 //
@@ -157,18 +162,18 @@
 //
 ////    NSArray *colors = [NSArray arrayWithObjects:(__bridge id)startColor, (__bridge id)endColor, nil];
 //    NSArray *colors = [NSArray arrayWithObjects:[UIColor colorWithCGColor:startColor], [UIColor colorWithCGColor:endColor], nil];
-//    
+//
 //    CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef) colors, locations);
-//    
+//
 //    CGPoint startPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMinY(rect));
 //    CGPoint endPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
-//    
+//
 //    CGContextSaveGState(context);
 //    CGContextAddRect(context, rect);
 //    CGContextClip(context);
 //    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
 //    CGContextRestoreGState(context);
-//    
+//
 //    CGColorSpaceRelease(colorSpace);
 //    CGGradientRelease(gradient);
 //}
