96811d939660ccdf177f39789c5c36ddb5be152b
Rishabh Tayal <rtayal11@gmail.com>
2013-10-08 17:10:19 -0500

    warning fix.

--- iOS/Wed/PRAnnotation.h
+++ iOS/Wed/PRAnnotation.h
@@ -11,7 +11,7 @@
 
 @interface PRAnnotation : NSObject<MKAnnotation>
 
-@property (nonatomic, strong) NSString* title;
+@property (nonatomic, copy) NSString* title;
 @property (nonatomic, strong) NSString* address;
 @property (nonatomic, assign) CLLocationCoordinate2D coordinate;
 
